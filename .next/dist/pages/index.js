356e6f413750aae915ef70761c4119b0cff35742
TH10\puvanachp <puvanach@sanook.com>
2018-01-08 19:04:42 +0700

    add framework

---
+++ .next/dist/pages/index.js
@@ -0,0 +1,27 @@
+'use strict';
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+
+var _react = require('react');
+
+var _react2 = _interopRequireDefault(_react);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+var _jsxFileName = 'D:\\git\\reactcourse\\pages\\index.js?entry';
+
+
+function HomePage() {
+
+  return _react2.default.createElement('div', {
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 6
+    }
+  }, 'Hi');
+}
+
+exports.default = HomePage;
+//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbInBhZ2VzXFxpbmRleC5qcyJdLCJuYW1lcyI6WyJSZWFjdCIsIkhvbWVQYWdlIl0sIm1hcHBpbmdzIjoiOzs7Ozs7QUFBQSxBQUFPOzs7Ozs7Ozs7QUFFUCxTQUFBLEFBQVMsV0FBVyxBQUVsQjs7eUJBQ0UsY0FBQTs7Z0JBQUE7a0JBQUE7QUFBQTtBQUFBLEdBQUEsRUFERixBQUNFLEFBS0g7QUFHRDs7a0JBQUEsQUFBZSIsImZpbGUiOiJpbmRleC5qcz9lbnRyeSIsInNvdXJjZVJvb3QiOiJEOi9naXQvcmVhY3Rjb3Vyc2UifQ==
---
+++ pages/index.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function HomePage() {
+	
+  return (
+    <div>
+       Hi
+    </div>
+  )
+  
+}
+
+
+export default HomePage;
